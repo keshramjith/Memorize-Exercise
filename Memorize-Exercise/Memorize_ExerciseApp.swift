0c1b0b8713f9628e99b92311289870ec252a94a8
Kesh Ramjith <warpple@protonmail.com>
2022-02-09 22:47:15 +0200

    Initial Commit

---
+++ Memorize-Exercise/Memorize_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_ExerciseApp.swift
+//  Memorize-Exercise
+//
+//  Created by Kesh Ramjith on 2022/02/09.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
